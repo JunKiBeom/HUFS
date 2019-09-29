e70cc864c7bdcef3403420ababf5ecb7040ae55b
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2019-09-30 03:23:13 +0900

    create /xv6-hw1

---
+++ 2019-2/운영체제 (Operating System)/xv6-hw1/snice.c
@@ -0,0 +1,28 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[])
+{
+  int pid, nice;
+
+  if (argc < 3){
+    printf(1,"Usage : snice pid nice \n");
+    exit();
+  }
+
+  //printf(1,"%c %c\n",argv[2][0],argv[2][1]);
+  if (argv[2][0] == '-'){
+    printf(1,"Invalid nice (0~40)\n-1\n");
+    exit();
+  }
+
+  pid = atoi (argv[1]);
+  nice = atoi (argv[2]);
+
+  printf(1,"pid=%d, nice=%d\n",pid,nice);
+  printf(1,"%d\n",snice(pid,nice));
+
+  exit();
+}
