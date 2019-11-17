0f7452def6449e3045606d4a3c85f158829c9fb7
JunKiBeom <dona0408@gmail.com>
2019-11-17 20:53:23 +0900

    Update semaphore.c

--- 2019-2/운영체제 (Operating System)/xv6-hw3/semaphore.c
+++ 2019-2/운영체제 (Operating System)/xv6-hw3/semaphore.c
@@ -9,6 +9,7 @@
 #define NSEMS			32 // 32 entries for semophore table
 #define MAX_WAITERS		64 // maximum no of waiters
 
+
 extern struct {
 	struct spinlock lock;
 	struct proc proc[NPROC];
@@ -22,7 +23,8 @@ struct semaphore {
 } sem[NSEMS];
 
 // function to initiallize the semophore table (at booting time)
-// You don't need call this seminit function!! 
+// You don't need call this seminit function!!
+
 void
 seminit(void)
 {
@@ -34,20 +36,21 @@ seminit(void)
 		sem[i].active = 0;
 		sem[i].value = 0;
 
-		for (j=0; j<MAX_WAITERS; j++) sem[i].waiters[j] = -1; // HUFS
+		for (j=0; j<MAX_WAITERS; j++)
+			sem[i].waiters[j] = -1; // HUFS
 
 	}
 }
 
-int 
+int
 sem_create(int max)
-{ 
+{
 	int i;
 
 	// find an entry is NOT active (not used)
 	for (i=0; i<NSEMS; i++) {
 		acquire(&sem[i].lock);
-		if (sem[i].active == 0) { 
+		if (sem[i].active == 0) {
 			sem[i].value = max;
 			sem[i].active = 1; // mark it as used (will be)
 			release(&sem[i].lock);
@@ -68,7 +71,7 @@ sem_destroy(int num)
 
 	acquire(&sem[num].lock);
 	// check if the entry is actived
-	if(sem[num].active != 1) 
+	if(sem[num].active != 1)
 		return -1;
 	sem[num].active = 0;
 	release(&sem[num].lock);
@@ -79,16 +82,91 @@ sem_destroy(int num)
 /*
    You have to just fill up the following functions!!!
 */
+
+int enqueue(struct semaphore *sem, int pid){
+	int i;
+
+	for (i=0; i<MAX_WAITERS; i++){
+		if (sem->waiters[i]==-1){
+			sem->waiters[i] = pid;
+			return 0;
+		}
+	}
+	return -1;
+}
+
+int dequeue(struct semaphore *sem){
+	int i,pid;
+
+	for (i=0; i<MAX_WAITERS; i++){
+		if ((pid=sem->waiters[i])!=-1){
+			sem->waiters[i]=-1;
+			return pid;
+		}
+	}
+	return -1;
+}
+
 int
 sem_wait(int num)
 {
+	acquire(&sem[num].lock);
 
-	return 0;
+	if(sem[num].active == 0) {
+		release(&sem[num].lock);
+		return -1;
+	}
+
+	sem[num].value--;
+
+	if (sem[num].value < 0){
+		if (enqueue(&sem[num], proc->pid)==-1) {
+			release(&sem[num].lock);
+			return -1;
+		}
+		sleep(&sem[num],&sem[num].lock);
+		// block(&sem[num].lock);
+
+		release(&sem[num].lock);
+		return 0;
+	}
+	else {
+		release(&sem[num].lock);
+		return 0;
+	}
 }
 
 int
 sem_signal(int num)
 {
+	acquire(&sem[num].lock);
 
-	return 0;
+	if(sem[num].active == 0)
+		return -1;
+
+	sem[num].value++;
+	if (sem[num].value<=0){
+
+		int pid = dequeue(&sem[num]);
+
+		if (pid == -1) {
+			release(&sem[num].lock);
+			return -1;
+		}
+
+		// wakeup(&sem[num]);
+
+		int ppid = wakeup_pid(pid, &sem[num].lock);
+		if (ppid == -1) {
+			release(&sem[num].lock);
+			return -1;
+		}
+
+		release(&sem[num].lock);
+        	return 0;
+	}
+	else {
+		release(&sem[num].lock);
+		return 0;
+	}
 }
